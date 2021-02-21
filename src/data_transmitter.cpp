7c7cc5e3e7073e120d6e0e4403285719d8e90d3c
0xBYTESHIFT <0xbyteshift@gmail.com>
2021-02-21 16:31:50 +0300

    updated network

--- src/data_transmitter.cpp
+++ src/data_transmitter.cpp
@@ -1,9 +1,25 @@
 #include "../include/data_transmitter.h"
+#include <xtensor/xadapt.hpp>
+
+namespace net = boost::asio;
+
+template<class Vec>
+static auto to_buf(const Vec &data){
+    return net::buffer(data.data(), sizeof(typename Vec::value_type)*data.size());
+}
+
+template<class Sock, class Vec>
+static auto send(Sock &socket, const Vec &data){
+    auto len = (uint64_t)sizeof(typename Vec::value_type)*data.size();
+    std::vector<uint64_t> len_vec{ std::move(len) };
+    net::write(socket, to_buf(len_vec), net::transfer_all());
+    net::write(socket, to_buf(data), net::transfer_all());
+}
 
 void data_transmitter::attatch_domain(std::shared_ptr<domain> dom){
     this->dom = dom;
-    shape.push_back(dom->get_mesh().rho_mesh.shape()[0]);
-    shape.push_back(dom->get_mesh().rho_mesh.shape()[1]);
+    shape.emplace_back(dom->get_mesh().rho_mesh.shape().at(0));
+    shape.emplace_back(dom->get_mesh().rho_mesh.shape().at(1));
 };
 
 void data_transmitter::accept_connection(){
@@ -21,19 +37,16 @@ void data_transmitter::send_data(){
     xt::xarray<bool>& solid_map = dom->get_mesh().is_solid;
 
     // Send info about arrays
-    //std::vector<int> shape;
-    //shape.push_back(rho_mesh.shape()[0]);
-    //shape.push_back(rho_mesh.shape()[1]);
-    boost::asio::write(socket, boost::asio::buffer(shape.data(),2) );//,boost::asio::transfer_all(), ignored_error); 
+    net::write(socket, to_buf(shape), net::transfer_all());
     std::cout << "Size info transmitted" << std::endl;
+    std::cout << "shape:" << xt::adapt(shape) << std::endl;
 
     // Send arrays
-    boost::asio::write(socket, boost::asio::buffer(rho_mesh.data(), rho_mesh.size()));
-    //                    boost::asio::transfer_all(), ignored_error); 
-    boost::asio::write(socket, boost::asio::buffer(vel_mesh.data(), vel_mesh.size()));
-    //                    boost::asio::transfer_all(), ignored_error);
-    boost::asio::write(socket, boost::asio::buffer(solid_map.data(), solid_map.size()));
-    //                    boost::asio::transfer_all(), ignored_error); 
+    send(socket, rho_mesh);
+    std::cout << "rho shape:" << xt::adapt(rho_mesh.shape()) << std::endl;
+    send(socket, vel_mesh);
+    std::cout << "vel shape:" << xt::adapt(vel_mesh.shape()) << std::endl;
+    send(socket, solid_map);
+    std::cout << "solid shape:" << xt::adapt(solid_map.shape()) << std::endl;
     std::cout << "Mesh content transmitted" << std::endl;
-
 };
